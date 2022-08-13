f3b308d641f4b8b211cf0fc3ffd9fc0698527d3d
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-08-13 15:13:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-import falcon
-import json 
+
+
+import json
 import pickle
 from wsgiref.simple_server import make_server
 
+import falcon
+import psycopg2
+import psycopg2.extras
+import logging
+import mylib
+import platform
+
+
+#Buliding ARTICLES database
+conn = psycopg2.connect(dbname = Articles, user= Ibrahim000, password = pass00pass, host = DB_Host)
+
+#Logging Info
+os_name = platform.uname()[0].lower()
+if os_name == "windows":
+   time = get_win_login_time()
+   date = get_win_login_date()
+
+    
+
+
 def classify_text(text):
-    file_name = 'resources/svm_modle.sav'
-    loaded_model = pickle.load(open(file_name, 'rb'))
-    predicted_svm_loaded = loaded_model.predict([article_body])
-    return predicted_svm_loaded[0]
+       file_name = 'resources/svm_modle.sav'
+       loaded_model = pickle.load(open(file_name, 'rb'))
+       predicted_svm_loaded = loaded_model.predict([article_body])
+       return predicted_svm_loaded[0]
         
 
 class ClassificationResource:
+    
     def on_post(self, req, resp):
         raw_input = req.bounded_stream.read()
         article_body = raw_json
@@ -25,7 +47,17 @@ class ClassificationResource:
             predicted_category: predicted_category
         })
 
-       
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute('CREATE TABLE ARTICLES (Date integer, Date integer, Title text, Body text, Category Text);')
+    cur.execute('INSERT INTO ARTICLES (Date) Values(%d/%d/%d),(date)')
+    cur.execute('INSERT INTO ARTICLES (time) Values(%d/%d/%d),(time)')
+    cur.execute('INSERT INTO ARTICLES (Body) Values(%s),(article_body)')
+    cur.execute('INSERT INTO ARTICLES (Category) Values(%s),(predicted_category)')
+
+
+    conn.commit()
+
+
 
 
 app = falcon.App()
@@ -34,3 +66,6 @@ app.add_route('/predict_category', ClassificationResource())
 if __name__ == '_main_':
     with make_server(',8000,app') as httpd:
         print('server is running on port 8000...')
+
+cur.close()
+conn.close()
